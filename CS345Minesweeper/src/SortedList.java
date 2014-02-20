ef09cbdd87a543dc9038a5fc85c52039683bd29c
Tejas <tejas89@gmail.com>
2014-02-20 01:00:21 -0800

    Initial implementation of IntervalList and SortedList using TreeMap and TreeSet respectivly

--- CS345Minesweeper/src/SortedList.java
+++ CS345Minesweeper/src/SortedList.java
@@ -1,4 +1,36 @@
+import java.util.TreeSet;
+import java.util.NavigableSet;
 
 public class SortedList {
+	TreeSet<Integer> Data;
+	
+	// construct an empty SortedList
+	public SortedList() {
+		Data = new TreeSet();
+	}
+	
+	// find if an integer exists in the SortedList
+	public boolean Find(Integer v){
+		return Data.contains(v);
+	}
+	
+	// return the smallest x such that x >= v
+	public Integer FindLub(Integer v){
+		return Data.ceiling(v);
+	}
+
+	public Insert(Integer v){
+		Data.add(v);
+	}
+	
+	public Delete(Integer v){
+		Data.remove(v);
+	}
+
+	// delete all integers from l to v, exclusive
+	public DeleteInterval(Integer l, Integer r){
+		NavigableSet interval = Data.subSet(l, false, r, false);
+		interval.clear();
+	}
 
 }
