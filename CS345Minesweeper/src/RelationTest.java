9f32223dd3e01b59f2ed765f9e2077df4201d23e
yonathan perez <yonathanperez@rescomp-13-272474.stanford.edu>
2014-03-09 14:10:51 -0700

    restored class RelationTest.java

---
+++ CS345Minesweeper/src/RelationTest.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class RelationTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> schema = new ArrayList<String>();
+		schema.add("A");
+		schema.add("B");
+		Relation R = null;
+		try {
+			R = new Relation(schema, "testdata.tsv");
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		R.Dump();
+	}
+
+}
