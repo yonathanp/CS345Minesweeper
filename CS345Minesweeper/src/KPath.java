9a39964e7283f4a17f0ccf72ac02c033b497976b
Tejas <tejas89@gmail.com>
2014-03-11 22:12:09 -0700

    add hash join to kpath query

--- CS345Minesweeper/src/KPath.java
+++ CS345Minesweeper/src/KPath.java
@@ -22,7 +22,39 @@ public class KPath {
 			Relation R = new Relation(schema, EdgeFile);
 			Relations.add(R);
 		}
+		
+		long startTime = System.nanoTime();
 		Minesweeper MS = new Minesweeper(Relations, GAO);
-		System.out.println("Found " + MS.Join(Relations).GetSize() + " k paths!");
+		long endTime = System.nanoTime();
+		System.out.println("Minesweeper Index Creation: " + (endTime - startTime)/1000000000.0);
+		
+		
+		startTime = System.nanoTime();
+		Relation MineOut = MS.Join(Relations);
+		endTime = System.nanoTime();
+		System.out.println("Minesweeper Join: " + (endTime - startTime)/1000000000.0);
+		
+		System.out.println("Found " + MineOut.GetSize() + " k paths!");
+
+		Relation LastRelation = Relations.get(0);
+		long indexTime = 0;
+		long joinTime = 0;
+
+		for ( int i = 1; i < Relations.size(); i++){
+			HashJoin H = new HashJoin(LastRelation, Relations.get(i), GAO);
+			startTime = System.nanoTime();
+			H.CreateIndex();
+			endTime = System.nanoTime();
+			indexTime += endTime - startTime;
+			
+			startTime = System.nanoTime();
+			LastRelation = H.JoinR();
+			endTime = System.nanoTime();
+			joinTime += endTime - startTime;
+		}
+		System.out.println("Hash Index Creation: " + (indexTime)/1000000000.0);
+		System.out.println("Hash Join: " + (joinTime)/1000000000.0);
+		
+		System.out.println("Found " + LastRelation.GetSize() + " k paths!");
 	}
 }
